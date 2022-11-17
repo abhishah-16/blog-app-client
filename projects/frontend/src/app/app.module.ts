95835c3125b814e0618dbe0226f0bc04053c1c11
abhishah <abhishah0961@gmail.com>
2022-11-17 14:42:08 +0530

    Create: Frontend components

--- projects/frontend/src/app/app.module.ts
+++ projects/frontend/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { SinglePostComponent } from './components/single-post/single-post.component';
+import { HomeComponent } from './components/home/home.component';
+import { CategoryListComponent } from './components/category-list/category-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    SinglePostComponent,
+    HomeComponent,
+    CategoryListComponent
   ],
   imports: [
     BrowserModule,

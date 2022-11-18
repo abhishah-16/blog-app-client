8ccc9ae111181ea91dcc4e07e581d131cea87fa6
abhishah <abhishah0961@gmail.com>
2022-11-18 16:16:48 +0530

    Create: all components of backend

--- projects/backend/src/app/app.module.ts
+++ projects/backend/src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MainComponent } from './components/main/main.component';
+import { PostComponent } from './components/post/post.component';
+import { AllpostComponent } from './components/post/allpost/allpost.component';
+import { NewpostComponent } from './components/post/newpost/newpost.component';
+import { EditpostComponent } from './components/post/editpost/editpost.component';
+import { CategoryComponent } from './components/category/category.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    DashboardComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent,
+    PostComponent,
+    AllpostComponent,
+    NewpostComponent,
+    EditpostComponent,
+    CategoryComponent
   ],
   imports: [
     BrowserModule,
